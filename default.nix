01c853d44cd37b7456c566f2f1076b73cce6e465
rx342 <15484682+rx342@users.noreply.github.com>
2025-07-07 19:10:11 +0200

    feat: runCommand -> runCommandNoCC

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@
 
 {
   libvitamtp = pkgs.callPackage ./libvitamtp.nix { inherit vitamtp; };
-  udev = pkgs.runCommand "psvita-udev" { } ''
+  udev = pkgs.runCommandNoCC "psvita-udev" { } ''
     install -D -m644 ${vitamtp}/debian/libvitamtp5.udev $out/lib/udev/rules.d/60-psvita.rules
   '';
 }
